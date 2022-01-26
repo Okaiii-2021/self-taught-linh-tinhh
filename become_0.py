70bc82ac74397f5986c13d7115c6d21cab3de729
Do Dinh Van <87236474+Okaiii-2021@users.noreply.github.com>
2022-01-26 09:11:43 +0700

    Update become_0.py

--- become_0.py
+++ become_0.py
@@ -9,14 +9,27 @@ a = [
 [0,0,0,0],
 [1,9,3,4,5,8],
 [1,2,3,1,5,6],
-[0,0,0,0],]
+[0,0,0,0],
+[1,2,3,4,5,8],
+[0,0,0,0,0],
+[1,2,3]]
 
-print(len(a))
-print(len(a[1]))
+b = []
+k = 0
 
+
+# scan
 for i in range(len(a)):
     for v in range(len(a[i])):
-        a[i][v] = 0
+        if a[i][v] == 0:
+            continue
+        else:
+            b.append(i)
+            break
 
+for c in range(len(b)):
+    a.remove(a[b[c]-k])
+    k += 1
+    print(a)
+    print("------------------------------")
 print(a)
-
